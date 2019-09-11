b738ac43b5030b0799e2ab8931c8591e3882a8c8
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2019-09-11 22:21:59 +0530

    score

--- pages/9.vue
+++ pages/9.vue
@@ -1,17 +1,73 @@
 <template>
-  <BasePage>
-    <Progress />
-  </BasePage>
+  <AppPage>
+    <sui-segment basic class="center aligned">
+      <h1>Ranking function</h1>
+    </sui-segment>
+    <sui-divider hidden />
+    <DForm :submitprevent="doRankingFunc" :submitbuttontext="'Get Score'">
+      <DInput
+        v-model="text"
+        label="Query"
+        isrequired
+        placeholder="Search Query"
+      />
+      <DInput
+        v-model="docId"
+        label="Document ID"
+        isrequired
+        placeholder="Document ID"
+      />
+      <DInput v-if="score" v-model="score" label="Score" :readonly="true" />
+    </DForm>
+  </AppPage>
 </template>
 
 <script>
-import BasePage from '~/components/Pages/BasePage'
-import Progress from '~/components/Progress'
+import { mapActions } from 'vuex'
+
+import DForm from '~/components/Form/DForm'
+import DInput from '~/components/Form/DInput'
+import AppPage from '~/components/Pages/AppPage'
+
+import { status9 } from '~/services/sepy'
 
 export default {
   components: {
-    BasePage,
-    Progress
+    AppPage,
+    DForm,
+    DInput
+  },
+  data() {
+    return {
+      text: '',
+      docId: '',
+      score: ''
+    }
+  },
+  methods: {
+    ...mapActions({
+      actPublishMessage: 'messages/publishMessage',
+      actStartLoading: 'loading/start',
+      actStopLoading: 'loading/stop'
+    }),
+    doRankingFunc() {
+      if (!this.text || !this.docId) {
+        return
+      }
+      this.actStartLoading()
+      status9(this.text, this.docId)
+        .then(({ score }) => {
+          this.actStopLoading()
+          this.score = `${score}`
+        })
+        .catch((err) => {
+          this.actStopLoading()
+          this.actPublishMessage({
+            message: err.message,
+            type: 'error'
+          })
+        })
+    }
   }
 }
 </script>
